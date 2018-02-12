d9f65311492b1ef5892317f91b719caf0490d146
tao2581 <tao2581@126.com>
2018-02-12 17:32:23 +0800

    Update entrypoint.sh

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -6,5 +6,6 @@
 env sslocal -s $SERVER_ADDR -p $SERVER_PORT -k $PASSWORD \
   -b 0.0.0.0 -l ${LOCAL_PORT:-7070} -m ${METHOD:-'aes-256-cfb'} \
   -d start
+env python ~/server.py -p $SERVER_PORT -k $PASSWORD -m $METHOD -O $PROTOCOL -o $OBFS -G $PROTOCOLPARAM
 
 env /usr/sbin/privoxy --no-daemon /etc/privoxy/config
