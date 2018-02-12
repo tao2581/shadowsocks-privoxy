ee58a8795706b14ff4bba49c9543fc3e910edfca
tao2581 <tao2581@126.com>
2018-02-12 18:11:09 +0800

    Update entrypoint.sh

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -3,6 +3,6 @@
 #------------------------------------------------------------------------------
 # Configure the service:
 #------------------------------------------------------------------------------
-env python /~/shadowsocksr-manyuser/shadowsocks/server.py -p $SERVER_PORT -k $PASSWORD -m $METHOD -O $PROTOCOL -o $OBFS -G $PROTOCOLPARAM
+env python /~/shadowsocksr-manyuser/shadowsocks/server.py -s $SERVER_ADDR -p $SERVER_PORT -k $PASSWORD -m $METHOD -O $PROTOCOL -o $OBFS -G $PROTOCOLPARAM
 
 env /usr/sbin/privoxy --no-daemon /etc/privoxy/config
